4b253b06b5ef4cb1477f99d5ea2af6267e6dd0df
Quitch <paulmcocker@gmail.com>
2015-06-24 21:01:43 +0100

    All five personalities
    Uploading the latest copy of the file due to AI Showdown issue -
    https://github.com/JustinLove/ai_showdown/issues/4#issuecomment-114990935

--- README.md
+++ README.md
@@ -8,28 +8,67 @@ This mod adds additional personalities (also incorrectly called difficulty level
 ## PERSONALITIES
 
 - Bronze
--- Limited number of platoons
--- One planet orbital
--- Techs early
--- Expands poorly
+-- Economy first opening
+-- One army
+-- Techs as soon as possible
+-- Goes orbital as soon as possible
+-- Turtles
 -- Poor troop selection
--- Scouts poorly
+-- Doesn't scout
+-- Doesn't react to what the enemy is doing
+-- Loves fabbers
+-- Loves static defence/offence
+-- Poor use of fabbers
+-- Doesn't think about running costs
+-- No micro
 
 - Silver
--- Limited number of platoons
--- One planet orbital
+-- Factory first opening
+-- Few armies
 -- Techs early
--- Expands poorly
+-- Goes orbital early
+-- Expands slowly
 -- Poor troop selection
+-- Barely scouts
+-- Doesn't react to what the enemy is doing
+-- Likes fabbers
+-- Floats economy
+-- No micro
 
 - Gold
+-- Smart factory first opening
+-- Some armies
+-- Prefers T1
+-- Expands OK
+-- Good troop selection
+-- OK scouting
+-- Some reaction to opponent's play
+-- Dislikes fabbers
+-- Stalls economy
+-- Average micro
 
 - Platinum
+-- Smart factory first opening
+-- Numerous armies
+-- Techs smartly
+-- Goes orbital smartly
+-- Expands quickly
+-- Excellent troop selection
+-- Smartly reacts to opponent's play
+-- Good economy handling
+-- Best micro
 
 - Uber
--- Plays the best game Queller can
+-- Smart factory first opening
+-- Unlimited armies
+-- Techs smartly
+-- Goes orbital smartly
+-- Expands quickly
+-- Excellent troop selection
+-- Smartly reacts to opponent's play
+-- Great economy handling
+-- Best micro
 
 ## THANKS TO
 
-- wondible, whose AI Personalities mod setup I took wholesale to act as a basis for this mod
 - Sorian, without whom there wouldn't be the cool features necessary to make this mod work
---
+++ ui/main/game/new_game/js/ai.js
@@ -0,0 +1,121 @@
+
+function ai_types() {
+
+    /* 
+    {
+        idle: true | false,
+
+        percent_vehicle [0 1],
+		percent_bot [0 1],
+        percent_air [0 1],
+        percent_naval [0 1],
+		percent_orbital [0 1],
+        
+        personality_tag: <string>,
+
+        metal_drain_check [0 1],
+        energy_drain_check [0 1],
+        metal_demand_check [0 1],
+        energy_demand_check [0 1],
+
+        micro_type 0:none | 1:platoon | 2:squad,
+
+        go_for_the_kill: true | false,
+        priority_scout_metal_spots: true | false,
+        enable_commander_danger_responses: true | false,
+        
+        neural_data_mod: [0 ... ]
+        adv_eco_mod: [0 ... ]
+        adv_eco_mod_alone: [0 ... ]
+        factory_build_delay_min: [0 ... ] (in seconds)
+        factory_build_delay_max: [0 ... ] (in seconds)
+		unable_to_expand_delay: [0 ... ] (in seconds)
+    } 
+    */
+
+    var result = {
+        'Idle': {
+            idle: true
+        },
+        'Bronze': {
+            metal_drain_check: 0.92,
+            energy_drain_check: 1.05,
+            metal_demand_check: 1.12,
+            energy_demand_check: 1.20,
+            micro_type: 0,
+            go_for_the_kill: false,
+            priority_scout_metal_spots: true,
+            enable_commander_danger_responses: false,
+            neural_data_mod: 1.3,
+            adv_eco_mod: 1.1,
+            adv_eco_mod_alone: 1.0,
+			factory_build_delay_min: 0,
+			factory_build_delay_max: 12,
+            personality_tag: "bronze"
+        },
+        'Silver': {
+            metal_drain_check: 0.72,
+            energy_drain_check: 0.85,
+            metal_demand_check: 0.92,
+            energy_demand_check: 1.0,
+            micro_type: 0,
+            go_for_the_kill: false,
+            priority_scout_metal_spots: true,
+            enable_commander_danger_responses: true,
+            neural_data_mod: 1.2,
+            adv_eco_mod: 1.1,
+            adv_eco_mod_alone: 1.0,
+			factory_build_delay_min: 0,
+			factory_build_delay_max: 3,
+            personality_tag: "silver"
+        },
+        'Gold': {
+            metal_drain_check: 0.47,
+            energy_drain_check: 0.6,
+            metal_demand_check: 0.67,
+            energy_demand_check: 0.75,
+            micro_type: 1,
+            go_for_the_kill: true,
+            priority_scout_metal_spots: true,
+            enable_commander_danger_responses: true,
+            neural_data_mod: 1.1,
+            adv_eco_mod: 1.5,
+            adv_eco_mod_alone: 0.95,
+            personality_tag: "gold"
+        },
+        'Platinum': {
+            metal_drain_check: 0.57,
+            energy_drain_check: 0.7,
+            metal_demand_check: 0.77,
+            energy_demand_check: 0.85,
+            micro_type: 2,
+            go_for_the_kill: true,
+            priority_scout_metal_spots: true,
+            enable_commander_danger_responses: true,
+            neural_data_mod: 1.0,
+            adv_eco_mod: 1.3,
+            adv_eco_mod_alone: 0.85,
+            personality_tag: "platinum"
+        },		
+        'Uber': {
+            metal_drain_check: 0.52,
+            energy_drain_check: 0.65,
+            metal_demand_check: 0.72,
+            energy_demand_check: 0.8,
+            micro_type: 2,
+            go_for_the_kill: true,
+            priority_scout_metal_spots: true,
+            enable_commander_danger_responses: true,
+            neural_data_mod: 1.0,
+            adv_eco_mod: 1.3,
+            adv_eco_mod_alone: 0.85,
+            personality_tag: "uber"
+        },
+    };
+
+    _.forEach(result, function (element, key) {
+        result[key]['name'] = key;
+    });
+
+    return result;
+}
