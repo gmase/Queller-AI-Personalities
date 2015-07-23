de75f81523e1c54611e881b0913a51f244c2ed2f
Quitch <paulmcocker@gmail.com>
2015-07-23 22:03:24 +0100

    Refinements
    Latest refinements to personalities. Removed legion until these are
    completed.

--- ui/main/game/new_game/js/ai.js
+++ ui/main/game/new_game/js/ai.js
@@ -70,7 +70,7 @@ function ai_types() {
             personality_tag: "silver"
         },
         'Vanilla Gold': {
-            metal_drain_check: 0.54,
+            metal_drain_check: 0.49,
             energy_drain_check: 0.52,
             metal_demand_check: 0.67,
             energy_demand_check: 0.67,
@@ -84,7 +84,7 @@ function ai_types() {
             personality_tag: "gold"
         },
         'Vanilla Platinum': {
-            metal_drain_check: 0.54,
+            metal_drain_check: 0.52,
             energy_drain_check: 0.67,
             metal_demand_check: 0.82,
             energy_demand_check: 0.82,
@@ -98,7 +98,7 @@ function ai_types() {
             personality_tag: "platinum"
         },
 		'Vanilla Uber': {
-            metal_drain_check: 0.52,
+            metal_drain_check: 0.54,
             energy_drain_check: 0.57,
             metal_demand_check: 0.72,
             energy_demand_check: 0.72,
@@ -111,68 +111,8 @@ function ai_types() {
             adv_eco_mod_alone: 0.85,
             personality_tag: "uber"
         },
-		'Legion Bronze': {
-            metal_drain_check: 0.92,
-            energy_drain_check: 1.05,
-            metal_demand_check: 1.12,
-            energy_demand_check: 1.20,
-            micro_type: 0,
-            go_for_the_kill: false,
-            priority_scout_metal_spots: true,
-            enable_commander_danger_responses: true,
-            neural_data_mod: 1.3,
-            adv_eco_mod: 1.1,
-            adv_eco_mod_alone: 1.0,
-			factory_build_delay_min: 0,
-			factory_build_delay_max: 12,
-            personality_tag: "legion_bronze"
-        },
-        'Legion Silver': {
-            metal_drain_check: 0.72,
-            energy_drain_check: 0.85,
-            metal_demand_check: 0.92,
-            energy_demand_check: 1.0,
-            micro_type: 1,
-            go_for_the_kill: false,
-            priority_scout_metal_spots: true,
-            enable_commander_danger_responses: true,
-            neural_data_mod: 1.2,
-            adv_eco_mod: 1.1,
-            adv_eco_mod_alone: 1.0,
-			factory_build_delay_min: 0,
-			factory_build_delay_max: 3,
-            personality_tag: "legion_silver"
-        },
-        'Legion Gold': {
+		'Vanilla Experimental': {
             metal_drain_check: 0.54,
-            energy_drain_check: 0.52,
-            metal_demand_check: 0.67,
-            energy_demand_check: 0.67,
-            micro_type: 2,
-            go_for_the_kill: true,
-            priority_scout_metal_spots: true,
-            enable_commander_danger_responses: true,
-            neural_data_mod: 1.1,
-            adv_eco_mod: 1.4,
-            adv_eco_mod_alone: 0.95,
-            personality_tag: "legion_gold"
-        },
-        'Legion Platinum': {
-            metal_drain_check: 0.54,
-            energy_drain_check: 0.67,
-            metal_demand_check: 0.82,
-            energy_demand_check: 0.82,
-            micro_type: 2,
-            go_for_the_kill: true,
-            priority_scout_metal_spots: true,
-            enable_commander_danger_responses: true,
-            neural_data_mod: 1.05,
-            adv_eco_mod: 1.3,
-            adv_eco_mod_alone: 0.85,
-            personality_tag: "legion_platinum"
-        },
-		'Legion Uber': {
-            metal_drain_check: 0.52,
             energy_drain_check: 0.57,
             metal_demand_check: 0.72,
             energy_demand_check: 0.72,
@@ -183,7 +123,7 @@ function ai_types() {
             neural_data_mod: 1.0,
             adv_eco_mod: 1.3,
             adv_eco_mod_alone: 0.85,
-            personality_tag: "legion_uber"
+            personality_tag: "experimental"
         },
     };
 
